4ed8b061900969bb072d3d1bb2bae7845a9f9312
Georgiy <kankiyagg@gmail.com>
2025-07-09 20:50:20 +0400

    commit

---
+++ CPP_01/ex04/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+typedef std::string str;
+
+void	getNewLine(str &line, str s1, str s2) 
+{
+	size_t	startPos = 0;
+
+	while ((startPos = line.find(s1, startPos)) != str::npos) 
+	{
+		line = line.substr(0, startPos) + s2 + line.substr(startPos + s1.length());
+		startPos += s2.length();
+	}
+}
+
+int	main(int ac, char **av) 
+{
+	if (ac != 4)
+	{
+		std::cout << "Error\n";
+		return (0);
+	}
+	
+	str				file = av[1];
+	str				s1 = av[2];
+	str				s2 = av[3];
+	str				line;
+	str				out_file = file + ".replace";
+	std::ifstream	input(file.c_str());
+	std::ofstream	output(out_file.c_str());
+	
+	if (input.is_open()) 
+	{
+		while (std::getline(input, line)) 
+		{
+			getNewLine(line, s1, s2);
+			output << line;
+			if (!input.eof())
+				output << std::endl;
+		}
+		input.close();
+		output.close();
+	}
+	else
+		std::cout << "Error\n" << std::endl;
+	return (0);
+}

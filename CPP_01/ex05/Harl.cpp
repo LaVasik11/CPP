e401a50a7e29bd85728ad1701d08448d4b32b182
Georgiy <kankiyagg@gmail.com>
2025-07-11 18:26:11 +0400

    commit

---
+++ CPP_01/ex05/Harl.cpp
@@ -0,0 +1,37 @@
+#include "Harl.hpp"
+
+void	Harl::debug() const
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void	Harl::info() const
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void	Harl::warning() const
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+}
+
+void	Harl::error() const
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void	Harl::complain(str level)
+{
+	void	(Harl::*arrayPtr[4])(void) const = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	str		arrayLevel[4] = {"debug", "info", "warning", "error"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (arrayLevel[i] == level)
+		{
+			(this->*arrayPtr[i])();
+			return;
+		}
+	}
+	std::cout << "Error.\n" << std::endl;
+}
